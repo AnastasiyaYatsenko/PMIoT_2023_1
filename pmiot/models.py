82fb5cb8cd80ecfd7660d3546c4e8cc1cb495e1e
KarnaukhVeronica <62516559+KarnaukhVeronica@users.noreply.github.com>
2023-11-13 17:49:34 +0000

    init

--- pmiot/models.py
+++ pmiot/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Measurement(models.Model):
     isWorking = models.BooleanField(default=False)
     value = models.IntegerField(max_length=50)
