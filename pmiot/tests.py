04d7447a76342dd7c85706d669f34a00e0453e4e
Mikelangelo64 <donni2001331@gmail.com>
2023-12-17 19:02:19 +0200

    added URLTest (Timofii)

--- pmiot/tests.py
+++ pmiot/tests.py
@@ -1,6 +1,9 @@
 import inspect
+import time
+
 from datetime import datetime
 
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.test import TestCase
 
@@ -143,7 +146,6 @@ class FunctionTests(TestCase):
         # check results
         self.assertEqual(len(res), 1)
 
-
 class NotificationTests(TestCase):
 
     # prepare data for every test
@@ -268,3 +270,38 @@ class DetailsViewsTests(TestCase):
         Archive.objects.all().delete()
         Measurement.objects.all().delete()
         User.objects.all().delete()
+
+class URLTest(TestCase):
+    def setUp(self):
+        # create 2 instances with different types
+        admin = User.objects.create_superuser(username='admin', password='admin')
+        measurement = Measurement.objects.create(measurementType='Temperature',
+                                                 image='/static/images/BMP085.jpg')
+        Archive.objects.create(sensor_id = measurement, value = 22.0, timestamp = datetime.now())
+    
+
+    def test_view_performance(self):
+
+        self.client.login(username='admin', password='admin')
+
+        # Вимірюємо час, який потрібен для здійснення запиту до views
+        start_time = time.time()
+        response = self.client.get('/about', follow=True)
+        end_time = time.time()
+
+        # Встановлюємо поріг для часу відповіді 
+        response_time_threshold = 0.5  # в секундах
+
+        # Перевіраємо чи час відповіді входить в допустимий діапазон
+        response_time = end_time - start_time
+        self.assertLessEqual(response_time, response_time_threshold)
+
+    def test_home_view_url(self):
+        
+        self.client.login(username='admin', password='admin')
+
+        # 
+        response = self.client.get('/', follow=True)
+
+        # 
+        self.assertEqual(response.status_code, 200)
