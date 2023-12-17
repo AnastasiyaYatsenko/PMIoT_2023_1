ee28f3b0010029bf882e260e1865cd9e4b0c7f36
Int3raptor <krivouszp@gmail.com>
2023-12-17 17:59:47 +0200

    2-nd attemp

--- pmiot/tests.py
+++ pmiot/tests.py
@@ -1,5 +1,8 @@
 import inspect
+import time
 from datetime import datetime
+
+from django.urls import reverse
 
 from django.contrib.auth.models import User
 from django.test import TestCase
@@ -8,64 +11,40 @@ from pmiot.models import Measurement, Archive
 from pmiot.scheduler.scheduler import data_from_dataset
 
 
-class FuncTest(TestCase):
-
-    def setUp(self):
+class URLTest(TestCase):
     
-        Measurement.objects.create(measurementType='Temperature')
-        Measurement.objects.create(measurementType='Humidity')
 
-        # output results
-        print()
-        print(inspect.getframeinfo(inspect.currentframe()).function)
-        print('Number of sensors:', len(Measurement.objects.all()))
+    def test_view_performance(self):
 
+        # Вимірюємо час, який потрібен для здійснення запиту до views
+        start_time = time.time()
+        response = self.client.get('/about')
+        end_time = time.time()
 
-    def test_function_with_valid_data(self):
-        # Test the function with valid input data
-                # change instances (isWorking = True)
-        measurement1 = Measurement.objects.get(measurementType='Temperature')
-        measurement1.isWorking = True
-        measurement1.save()
-        measurement2 = Measurement.objects.get(measurementType='Humidity')
-        measurement2.isWorking = True
-        measurement2.save()
-        
-        # run function
-        res = data_from_dataset()
+        # Перевіряємо статус відповіді HTTP
+        self.assertEqual(response.status_code, 200)
 
-        # output results
-        print()
-        print(inspect.getframeinfo(inspect.currentframe()).function)
-        print(res)
-        print('------------')
-        
-        # compare results
-        self.assertEqual(len(res), 2)
+        # Встановлюємо поріг для часу відповіді 
+        response_time_threshold = 0.5  # в секундах
 
-    def test_function_with_invalid_data(self):
-        # Test the function with invalid input data (e.g., empty dataset)
-        measurement1 = Measurement.objects.get(measurementType='Temperature')
-        measurement1.isWorking = False
-        measurement1.save()
-        measurement2 = Measurement.objects.get(measurementType='Humidity')
-        measurement2.isWorking = False
-        measurement2.save()
-        
-        # run function
-        res = data_from_dataset()
+        # Перевіраємо чи час відповіді входить в допустимий діапазон
+        response_time = end_time - start_time
+        self.assertLessEqual(response_time, response_time_threshold)
 
-        # print results
-        print()
-        print(inspect.getframeinfo(inspect.currentframe()).function)
-        print(res)
-        print('------------')
+        # виводимо результат
+        print(f"Response Time for about {response_time} seconds")
 
-        self.assertEqual(len(res), 0)
+    def test_home_view_url(self):
+        # 
+        response = self.client.get('/')
 
-    def tearDown(self):
-        Archive.objects.all().delete()
-        Measurement.objects.all().delete()
-        User.objects.all().delete()    
+        # 
+        self.assertEqual(response.status_code, 200)
+
+        # 
+        # self.assertTemplateUsed(response, 'pmiot/home.html')
+
+        # Output results
+        print(f"URL Routing Test Passed for {'/'}")    
 
 
